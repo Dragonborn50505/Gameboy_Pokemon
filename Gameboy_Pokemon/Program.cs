444920428ac70ec7f64781ec70a230b145106e7c
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2022-01-12 11:06:04 +0100

    Pokemon_Gameboy

--- Gameboy_Pokemon/Program.cs
+++ Gameboy_Pokemon/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Numerics;
+using Raylib_cs;
+using System.Collections.Generic;
 
-namespace Gameboy_Pokemon
-{
-    class Program
-    {
-        static void Main(string[] args)
+Raylib.InitWindow(800, 600, "The title of my window");
+Raylib.SetTargetFPS(60);
+
+string level = "lobby";
+
+
+while (!Raylib.WindowShouldClose())
+{   Raylib.BeginDrawing();
+    
+        if (level == "lobby")
         {
-            Console.WriteLine("Hello World!");
+            Raylib.ClearBackground(Color.YELLOW);
+            
         }
-    }
+
+        if (level == "fight")
+        {
+            Raylib.ClearBackground(Color.GRAY);
+        }
+    
+    
+    Raylib.EndDrawing();
 }
