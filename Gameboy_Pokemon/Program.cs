3571b176417292c3fd2fcf41ee9b622253cee1db
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2022-02-08 11:07:46 +0100

    we run away

--- Gameboy_Pokemon/Program.cs
+++ Gameboy_Pokemon/Program.cs
@@ -6,14 +6,33 @@ using System.Collections.Generic;
 
 Raylib.InitWindow(800, 600, "The title of my window");
 Raylib.SetTargetFPS(60);
-
+Random generator = new Random();
+string aiStart = "no";
 float speed = 6f;
 
+
+
+// Vector2 mousePos = Raylib.GetMousePosition();
+// if (Raylib.CheckCollisionPointRec(mousePos, ) && )
+// Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON)
 
 Rectangle playerRect = new Rectangle(100, 100, 50, 50);
 Rectangle doorRect = new Rectangle(260, 560, 40, 40);
 Rectangle doorRect2 = new Rectangle(300, 560, 40, 40);
 Rectangle bossRect = new Rectangle(700, 70, 40, 40);
+
+Rectangle bossFighter = new Rectangle(600, 100, 40, 40);
+Rectangle fighter = new Rectangle(100, 350, 40, 40);
+
+
+Raylib.DrawText("Use headbutt", 50, 500, 40, Color.LIGHTGRAY);
+Raylib.DrawText("Use Kick", 50, 550, 40, Color.LIGHTGRAY);
+Raylib.DrawText("Run Away", 400, 450, 40, Color.BLACK);
+
+Rectangle headbutt = new Rectangle(50, 500, 40, 40);
+Rectangle kick = new Rectangle(50, 550, 40, 40);
+Rectangle runAway = new Rectangle(400, 450, 40, 40);
+
 Rectangle lineHorisontelBossFight = new Rectangle(0, 400, 800, 5);
 Rectangle lineVerticalBossFight = new Rectangle(600, 400, 5, 200);
 Texture2D winterBackground = Raylib.LoadTexture("Vinterprojektet.png");
@@ -21,6 +40,7 @@ Texture2D winterBackground = Raylib.LoadTexture("Vinterprojektet.png");
 
 float time = 0;
 int fightersHp = 100;
+int hp_ai = 100;
 float blackAndWhite = 0;
 
 
@@ -142,7 +162,7 @@ while (!Raylib.WindowShouldClose())
     if (undoX == true) playerRect.x -= movement.X;
     if (undoY == true) playerRect.y -= movement.Y;
 
-    Raylib.BeginDrawing();
+    Raylib.BeginDrawing();{
 
     if (level == "start")
     {
@@ -170,13 +190,118 @@ while (!Raylib.WindowShouldClose())
 
     if (level == "bossfight" && blackAndWhite >= 120)
     {
+        Vector2 mousePos = Raylib.GetMousePosition();
         Raylib.ClearBackground(Color.YELLOW);
         Raylib.DrawText("ai name", 50, 50, 40, Color.LIGHTGRAY);
-        Raylib.DrawText("ai hp", 50, 100, 40, Color.LIGHTGRAY);
+        Raylib.DrawText($"{hp_ai}", 50, 100, 40, Color.LIGHTGRAY);
         Raylib.DrawText("name", 650, 450, 40, Color.LIGHTGRAY);
+        Raylib.DrawText("Attack:", 50, 450, 40, Color.BLACK);
+        Raylib.DrawText("Use headbutt", 50, 500, 40, Color.LIGHTGRAY);
+        Raylib.DrawText("Use Kick", 50, 550, 40, Color.LIGHTGRAY);
+        Raylib.DrawText("Run Away", 400, 450, 40, Color.BLACK);
         Raylib.DrawText($"{fightersHp}", 650, 500, 40, Color.LIGHTGRAY);
         Raylib.DrawRectangleRec(lineHorisontelBossFight, Color.BLACK);
         Raylib.DrawRectangleRec(lineVerticalBossFight, Color.BLACK);
+
+        Raylib.DrawRectangleRec(bossFighter, Color.RED);
+        Raylib.DrawRectangleRec(fighter, Color.BLUE);
+
+        // 
+        // if (Raylib.CheckCollisionPointRec(mousePos, ) && )
+        // Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON)
+        if (Raylib.CheckCollisionPointRec(mousePos, headbutt) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) && aiStart = "no")
+    {
+            {
+                int HitOrMissName = generator.Next(1, 11);
+
+                if (HitOrMissName == 1)
+                {
+                   aiStart = "yes";
+                }
+                else
+                {
+                    int damage = 10 + generator.Next(0, 21);
+                            hp_ai -= damage;
+                    damage = 0;
+                    aiStart = "yes";
+                }
+            }
+        }
+
+        if (Raylib.CheckCollisionPointRec(mousePos, kick) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) && aiStart = "no"){
+             int KickOrMissName = generator.Next(1, 5);
+
+            if (KickOrMissName == 1)
+            {
+            aiStart = "yes";
+            }
+            else
+            {
+                int damage = 20 + generator.Next(0, 26);
+                hp_ai -= damage;
+                damage = 0;
+                aiStart = "yes";
+            }
+        }
+         if (Raylib.CheckCollisionPointRec(mousePos, runAway) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) && aiStart = "no"){
+            level = "start";}
+
+        while (aiStart == "yes") {
+            //fights back'
+
+
+ int ai_choice = generator.Next(1, 6);
+
+        if (ai_choice == 1)
+        {
+            int KickOrMissAI = generator.Next(1, 12);
+
+            if (KickOrMissAI == 1)
+            {
+               
+            }
+            else
+            {
+                int damage = 20 + generator.Next(0, 26);
+                fightersHp -= damage;
+                damage = 0;
+            }
+        }
+
+        //________________________________________________________________________________
+
+        else
+        {
+            int HitOrMissAI = generator.Next(1, 21);
+
+            if (HitOrMissAI == 1)
+            {
+               
+            }
+
+            else
+            {
+                int damage = 10 + generator.Next(0, 21);
+                fightersHp -= damage;
+                damage = 0;
+            }
+        }
+            aiStart = "no";
+        }
+
+            if (hp_ai <= 0){
+                Raylib.ClearBackground(Color.BLUE);
+            }
+            if (fightersHp <= 0){
+                Raylib.ClearBackground(Color.RED);
+            }
+            
+
+        }
+       
+
+
+
     }
     if (level == "bossfight" && blackAndWhite < 120)
     {
@@ -210,51 +335,4 @@ static Vector2 ReadMovement(float speed)
 
 //static void CheckMovement(){
 
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Raylib.InitWindow(800, 600, "The title of my window");
-//Raylib.SetTargetFPS(60);
-
-//string level = "lobby";
-//bool undoX = false;
-//bool undoY = false;
-
-//while (!Raylib.WindowShouldClose())
-//{
-//   Raylib.BeginDrawing();
-
-
-
-//    if (level == "lobby")
-//   {
-//       Raylib.ClearBackground(Color.YELLOW);
-
-//   }
-
-//   if (level == "fight")
-//   {
-//       Raylib.ClearBackground(Color.GRAY);
-//   }
-
-
-//  Raylib.EndDrawing();
 //}
