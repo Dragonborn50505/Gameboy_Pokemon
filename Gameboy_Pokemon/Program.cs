934af49fd2f6b72b5a78bd8f7c9cd516797f03ab
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2022-02-09 11:06:32 +0100

    skickaIn

--- Gameboy_Pokemon/Program.cs
+++ Gameboy_Pokemon/Program.cs
@@ -7,14 +7,12 @@ using System.Collections.Generic;
 Raylib.InitWindow(800, 600, "The title of my window");
 Raylib.SetTargetFPS(60);
 Random generator = new Random();
-string aiStart = "no";
+string fight = "hasNotHappend";
 float speed = 6f;
-
-
-
-// Vector2 mousePos = Raylib.GetMousePosition();
-// if (Raylib.CheckCollisionPointRec(mousePos, ) && )
-// Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON)
+string turn = "player";
+int playerTimer = 0;
+int enemyTimer = 0;
+string potionHp = "inactive";
 
 Rectangle playerRect = new Rectangle(100, 100, 50, 50);
 Rectangle doorRect = new Rectangle(260, 560, 40, 40);
@@ -23,6 +21,8 @@ Rectangle bossRect = new Rectangle(700, 70, 40, 40);
 
 Rectangle bossFighter = new Rectangle(600, 100, 40, 40);
 Rectangle fighter = new Rectangle(100, 350, 40, 40);
+Rectangle shopkeeper = new Rectangle(600, 100, 40, 40);
+Rectangle potion = new Rectangle(50, 100, 40, 40);
 
 
 Raylib.DrawText("Use headbutt", 50, 500, 40, Color.LIGHTGRAY);
@@ -42,6 +42,7 @@ float time = 0;
 int fightersHp = 100;
 int hp_ai = 100;
 float blackAndWhite = 0;
+
 
 
 string level = "start";
@@ -162,148 +163,190 @@ while (!Raylib.WindowShouldClose())
     if (undoX == true) playerRect.x -= movement.X;
     if (undoY == true) playerRect.y -= movement.Y;
 
-    Raylib.BeginDrawing();{
-
-    if (level == "start")
+    Raylib.BeginDrawing();
     {
-        Raylib.ClearBackground(Color.BLUE);
-        Raylib.DrawRectangleRec(playerRect, Color.BROWN);
-        Raylib.DrawRectangleRec(doorRect, Color.BLACK);
-        Raylib.DrawRectangleRec(bossRect, Color.PURPLE);
-    }
 
-    else if (level == "outside")
-    {
-        Raylib.ClearBackground(Color.PINK);
-        Raylib.DrawTexture(winterBackground, 0, 0, Color.WHITE);
-        Raylib.DrawRectangleRec(playerRect, Color.BROWN);
-        Raylib.DrawRectangleRec(doorRect, Color.BLACK);
-        Raylib.DrawRectangleRec(doorRect2, Color.BLACK);
-    }
+        if (level == "start")
+        {
+            Raylib.ClearBackground(Color.BLUE);
+            Raylib.DrawRectangleRec(playerRect, Color.BROWN);
+            Raylib.DrawRectangleRec(doorRect, Color.BLACK);
+            Raylib.DrawRectangleRec(bossRect, Color.PURPLE);
+        }
 
-    else if (level == "shop")
-    {
-        Raylib.ClearBackground(Color.RED);
-        Raylib.DrawRectangleRec(playerRect, Color.BROWN);
-        Raylib.DrawRectangleRec(doorRect2, Color.BLACK);
-    }
+        else if (level == "outside")
+        {
+            Raylib.ClearBackground(Color.PINK);
+            Raylib.DrawTexture(winterBackground, 0, 0, Color.WHITE);
+            Raylib.DrawRectangleRec(playerRect, Color.BROWN);
+            Raylib.DrawRectangleRec(doorRect, Color.BLACK);
+            Raylib.DrawRectangleRec(doorRect2, Color.BLACK);
+        }
 
-    if (level == "bossfight" && blackAndWhite >= 120)
-    {
-        Vector2 mousePos = Raylib.GetMousePosition();
-        Raylib.ClearBackground(Color.YELLOW);
-        Raylib.DrawText("ai name", 50, 50, 40, Color.LIGHTGRAY);
-        Raylib.DrawText($"{hp_ai}", 50, 100, 40, Color.LIGHTGRAY);
-        Raylib.DrawText("name", 650, 450, 40, Color.LIGHTGRAY);
-        Raylib.DrawText("Attack:", 50, 450, 40, Color.BLACK);
-        Raylib.DrawText("Use headbutt", 50, 500, 40, Color.LIGHTGRAY);
-        Raylib.DrawText("Use Kick", 50, 550, 40, Color.LIGHTGRAY);
-        Raylib.DrawText("Run Away", 400, 450, 40, Color.BLACK);
-        Raylib.DrawText($"{fightersHp}", 650, 500, 40, Color.LIGHTGRAY);
-        Raylib.DrawRectangleRec(lineHorisontelBossFight, Color.BLACK);
-        Raylib.DrawRectangleRec(lineVerticalBossFight, Color.BLACK);
+        else if (level == "shop")
+        {
+            Raylib.ClearBackground(Color.RED);
+            Raylib.DrawRectangleRec(playerRect, Color.BROWN);
+            Raylib.DrawRectangleRec(doorRect2, Color.BLACK);
+            Raylib.DrawRectangleRec(shopkeeper, Color.PINK);
 
-        Raylib.DrawRectangleRec(bossFighter, Color.RED);
-        Raylib.DrawRectangleRec(fighter, Color.BLUE);
-
-        // 
-        // if (Raylib.CheckCollisionPointRec(mousePos, ) && )
-        // Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON)
-        if (Raylib.CheckCollisionPointRec(mousePos, headbutt) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) && aiStart == "no")
-    {
+            Vector2 mousePos = Raylib.GetMousePosition();
+            if (Raylib.CheckCollisionPointRec(mousePos, shopkeeper) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
             {
+                level = "buy";
+            }
+        }
+
+        else if (level == "buy")
+        {
+            Raylib.DrawText("potion", 50, 100, 40, Color.LIGHTGRAY);
+            potionHp = "active";
+            level =  "shop";
+        }
+
+        else if (level == "victory"){
+            Raylib.ClearBackground(Color.BLUE);
+            Raylib.DrawText("You Won!", 400, 280, 40, Color.BLACK);
+        }
+
+        else if (level == "game over"){
+            Raylib.ClearBackground(Color.RED);
+            Raylib.DrawText("You lost",  400, 280, 40, Color.BLACK);
+        }
+
+        if (level == "bossfight" && blackAndWhite >= 120 && fight == "hasNotHappend")
+        {
+            Vector2 mousePos = Raylib.GetMousePosition();
+            Raylib.ClearBackground(Color.YELLOW);
+            Raylib.DrawText("ai name", 50, 50, 40, Color.LIGHTGRAY);
+            Raylib.DrawText($"{hp_ai}", 50, 100, 40, Color.LIGHTGRAY);
+            Raylib.DrawText("name", 650, 450, 40, Color.LIGHTGRAY);
+            Raylib.DrawText("Attack:", 50, 450, 40, Color.BLACK);
+            Raylib.DrawText("Use headbutt", 50, 500, 40, Color.LIGHTGRAY);
+            Raylib.DrawText("Use Kick", 50, 550, 40, Color.LIGHTGRAY);
+            Raylib.DrawText("Run Away", 400, 450, 40, Color.BLACK);
+            Raylib.DrawText($"{fightersHp}", 650, 500, 40, Color.LIGHTGRAY);
+            Raylib.DrawRectangleRec(lineHorisontelBossFight, Color.BLACK);
+            Raylib.DrawRectangleRec(lineVerticalBossFight, Color.BLACK);
+
+            Raylib.DrawRectangleRec(bossFighter, Color.RED);
+            Raylib.DrawRectangleRec(fighter, Color.BLUE);
+                                                                                                        
+            // 
+            // if (Raylib.CheckCollisionPointRec(mousePos, ) && )
+            // Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON)
+            if (Raylib.CheckCollisionPointRec(mousePos, headbutt) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) && turn == "player" && playerTimer == 0)
+            {
+
                 int HitOrMissName = generator.Next(1, 11);
 
                 if (HitOrMissName == 1)
                 {
-                   aiStart = "yes";
+                    enemyTimer = 120;
+                    turn = "enemy";
                 }
                 else
                 {
                     int damage = 10 + generator.Next(0, 21);
-                            hp_ai -= damage;
+                    hp_ai -= damage;
                     damage = 0;
-                    aiStart = "yes";
+                    enemyTimer = 120;
+                    turn = "enemy";
+                }
+
+            }
+
+            if (Raylib.CheckCollisionPointRec(mousePos, kick) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
+            {
+                int KickOrMissName = generator.Next(1, 5);
+
+                if (KickOrMissName == 1)
+                {
+                    enemyTimer = 120;
+                    turn = "enemy";
+                }
+                else
+                {
+                    int damage = 20 + generator.Next(0, 26);
+                    hp_ai -= damage;
+                    damage = 0;
+                    enemyTimer = 120;
+                    turn = "enemy";
                 }
             }
-        }
-
-        if (Raylib.CheckCollisionPointRec(mousePos, kick) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) && aiStart == "no"){
-             int KickOrMissName = generator.Next(1, 5);
-
-            if (KickOrMissName == 1)
+            if (Raylib.CheckCollisionPointRec(mousePos, runAway) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
             {
-            aiStart = "yes";
+                level = "start";
             }
-            else
+
+            while (turn == "enemy" && enemyTimer == 0)
             {
-                int damage = 20 + generator.Next(0, 26);
-                hp_ai -= damage;
-                damage = 0;
-                aiStart = "yes";
-            }
-        }
-         if (Raylib.CheckCollisionPointRec(mousePos, runAway) && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) && aiStart == "no"){
-            level = "start";}
+                //fights back'
+        
+                    int ai_choice = generator.Next(1, 6);
 
-        while (aiStart == "yes") {
-            //fights back'
+                    if (ai_choice == 1)
+                    {
+                        int KickOrMissAI = generator.Next(1, 12);
 
+                        if (KickOrMissAI == 1)
+                        {
 
- int ai_choice = generator.Next(1, 6);
+                        }
+                        else
+                        {
+                            int damage = 20 + generator.Next(0, 26);
+                            fightersHp -= damage;
+                            damage = 0;
+                        }
+                    }
 
-        if (ai_choice == 1)
-        {
-            int KickOrMissAI = generator.Next(1, 12);
+                    //________________________________________________________________________________
 
-            if (KickOrMissAI == 1)
-            {
-               
-            }
-            else
-            {
-                int damage = 20 + generator.Next(0, 26);
-                fightersHp -= damage;
-                damage = 0;
-            }
-        }
+                    else
+                    {
+                        int HitOrMissAI = generator.Next(1, 21);
 
-        //________________________________________________________________________________
+                        if (HitOrMissAI == 1)
+                        {
 
-        else
-        {
-            int HitOrMissAI = generator.Next(1, 21);
+                        }
 
-            if (HitOrMissAI == 1)
-            {
-               
-            }
-
-            else
-            {
-                int damage = 10 + generator.Next(0, 21);
-                fightersHp -= damage;
-                damage = 0;
-            }
-        }
-            aiStart = "no";
-        }
-
-            if (hp_ai <= 0){
-                Raylib.ClearBackground(Color.BLUE);
-            }
-            if (fightersHp <= 0){
-                Raylib.ClearBackground(Color.RED);
-            }
+                        else
+                        {
+                            int damage = 10 + generator.Next(0, 21);
+                            fightersHp -= damage;
+                            damage = 0;
+                        }
+                    }
             
+                playerTimer = 120;
+                turn = "player";
+            }
+
+            if (playerTimer > 0) playerTimer--;
+            if (enemyTimer > 0) enemyTimer--;
+
+
+            if (hp_ai <= 0)
+            {
+                fight = "happend";
+                level = "victory";
+            }
+            if (fightersHp <= 0)
+            {
+                fight = "happend";
+                level = "game over";
+            }
+
 
         }
-       
+
 
 
 
     }
-    if (level == "bossfight" && blackAndWhite < 120)
+    if (level == "bossfight" && blackAndWhite < 120) //My favorite part of the code, beteween being a even and uneven number it changes back and fourth from black to white to stimulate playing pokemone on gameboye
     {
         if ((blackAndWhite / 10) % 2 == 0)
         {
@@ -316,6 +359,13 @@ while (!Raylib.WindowShouldClose())
         blackAndWhite++;
     }
 
+if (potionHp == "active" && hp_ai <= 0){ //Was planed to give the player a health boost if you bought it but were not able to make it work in time
+    int hp = 20;
+    fightersHp =+ hp;
+
+    hp--;
+
+}
 
     Raylib.EndDrawing();
 }
